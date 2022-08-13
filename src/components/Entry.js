7286ac94dedc705b737b9174761fa33f13437bc5
FoxSylv <atheriault@bowdoin.edu>
2022-08-12 21:27:13 -0400

    Add links to the entries.
    Also add some small style improvements to compensate for the
    space the links now take up.

--- src/components/Entry.js
+++ src/components/Entry.js
@@ -1,14 +1,27 @@
 import './Entry.scss';
 
 function Entry(props) {
-    return (
-        <div className="entry box">
-            <h3>
-                {props.data.title}
-            </h3>
-            <p>
-                {props.data.subtitle}
+    var links = props.data.links.map((link, index) =>
+        <a href={link.link} className="flex-element box" key={link.label}>
+            <p className="entry-link">
+                {link.label}
             </p>
+        </a>
+    );
+
+    return (
+        <div className="flex-element box">
+            <a href={(props.data.links[0]).link}>
+                <h3 className="entry-title">
+                    {props.data.title}
+                </h3>
+                <p className="entry-subtitle">
+                    {props.data.subtitle}
+                </p>
+            </a>
+            <div className="flex entry-links">
+                {links}
+            </div>
         </div>
     );
 }
--- src/data/entryData.js
+++ src/data/entryData.js
@@ -2,19 +2,19 @@ export const entryData = [
     {
         title: "title",
         subtitle: "subtitle",
-        links: [{label: "link1", link: "foxsylv1.github.io"},
-                {label: "link2", link: "foxsylv1.github.io/letter-getter"}]
+        links: [{label: "link1", link: "https://foxsylv1.github.io"},
+                {label: "link2", link: "https://foxsylv1.github.io/letter-getter"}]
     },
     {
         title: "title2",
         subtitle: "subtitle2",
-        links: [{label: "link3", link: "foxsylv1.github.io"},
-                {label: "link4", link: "foxsylv1.github.io/letter-getter"}]
+        links: [{label: "link3", link: "https://foxsylv1.github.io"},
+                {label: "link4", link: "https://foxsylv1.github.io/letter-getter"}]
     },
     {
         title: "title3",
         subtitle: "subtitle3",
-        links: [{label: "link5", link: "foxsylv1.github.io"},
-                {label: "link6", link: "foxsylv1.github.io/letter-getter"}]
+        links: [{label: "link5", link: "https://foxsylv1.github.io"},
+                {label: "link6", link: "https://foxsylv1.github.io/letter-getter"}]
     }
 ];
