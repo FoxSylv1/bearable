0a85c963050db952b661d2be2ee98c5cc53419df
FoxSylv <atheriault@bowdoin.edu>
2022-08-13 14:05:43 -0400

    Add all links.
    Add all links and relabel some to better fit
    the Bowdoin-provided resource.

--- src/data/entryData.js
+++ src/data/entryData.js
@@ -2,78 +2,79 @@ export const entryData = [
     {
         title: "classwork",
         subtitle: "view and submit schoolwork",
-        links: [{label: "canvas", link: "https://foxsylv1.github.io"},
-                {label: "blackboard", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "canvas", link: "https://bowdoin.instructure.com"},
+                {label: "blackboard", link: "https://blackboard.bowdoin.edu"},
+                {label: "gradescope", link: "https://www.gradescope.com"}]
     },
     {
         title: "outlook",
         subtitle: "view and send email",
-        links: [{label: "email", link: "https://foxsylv1.github.io"},
-                {label: "calendar", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "email", link: "https://outlook.office.com/mail/"},
+                {label: "calendar", link: "https://outlook.office.com/calendar/"}]
     },
     {
         title: "onecard",
         subtitle: "view your onecard data",
-        links: [{label: "meal swipes", link: "https://foxsylv1.github.io"},
-                {label: "money stored", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "meal swipes", link: "https://csgoldweb.bowdoin.edu"},
+                {label: "money stored", link: "https://csgoldweb.bowdoin.edu"}]
     },
     {
         title: "housing & mail",
         subtitle: "view your current residence/mailing address, or apply for housing",
-        links: [{label: "current", link: "https://foxsylv1.github.io"},
-                {label: "apply", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "current", link: "https://www.bowdoin.edu/directory/"},
+                {label: "apply", link: "https://starrez.bowdoin.edu"}]
     },
     {
         title: "dining services",
         subtitle: "view dining hall menus and hours",
-        links: [{label: "menu", link: "https://foxsylv1.github.io"},
-                {label: "hours", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "menu", link: "https://bowdoin.edu/dining/menus/"},
+                {label: "hours", link: "https://bowdoin.edu/dining/hours-and-locations/"}]
     },
     {
         title: "community",
         subtitle: "view what others are up to",
-        links: [{label: "campus groups", link: "https://foxsylv1.github.io"},
-                {label: "groupme", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "events", link: "https://bowdoin.campusgroups.com/web_app?id=24706&menu_id=55996"},
+                {label: "club list", link: "https://bowdoin.campusgroups.com/club_signup?view=all&"}]
     },
     {
-        title: "past courses",
-        subtitle: "view your transcript or request an audit",
-        links: [{label: "transcript", link: "https://foxsylv1.github.io"},
-                {label: "audit", link: "https://foxsylv1.github.io/letter-getter"}]
+        title: "courses",
+        subtitle: "view your current and previous courses",
+        links: [{label: "current", link: "https://luminis.bowdoin.edu"},
+                {label: "previous", link: "https://degreeworks.bowdoin.edu"}]
     },
     {
         title: "registration",
-        subtitle: "view and register for courses",
-        links: [{label: "register", link: "https://foxsylv1.github.io"},
-                {label: "class listing", link: "https://foxsylv1.github.io/letter-getter"}]
+        subtitle: "view and register for upcoming courses",
+        links: [{label: "register", link: "https://polaris.bowdoin.edu/Registration"},
+                {label: "class listing", link: "https://apphub.bowdoin.edu/classfinder/"}]
     },
     {
         title: "financial services",
         subtitle: "view payment and financial aid information",
-        links: [{label: "bill", link: "https://foxsylv1.github.io"},
-                {label: "financial aid", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "bill", link: "https://quikpayasp.com/bowdoin/quikpay/spa/viewandpayindex.do"},
+                {label: "financial aid", link: "https://netpartner.bowdoin.edu"}]
     },
     {
         title: "labor services",
-        subtitle: "submit time or view labor opportunities",
-        links: [{label: "submit time", link: "https://foxsylv1.github.io"},
-                {label: "apply", link: "https://foxsylv1.github.io/letter-getter"}]
+        subtitle: "view labor opportunities or submit time",
+        links: [{label: "apply", link: "https://studentemployment.bowdoin.edu/JobX_FindAJob.aspx"},
+                {label: "submit time", link: "https://www.myworkday.com/bowdoin/"}]
     }, 
     {
         title: "health",
         subtitle: "view supports for your well-being",
-        links: [{label: "mental", link: "https://foxsylv1.github.io"},
-                {label: "physical", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "mental", link: "https://bowdoin.edu/counseling/"},
+                {label: "physical", link: "https://bowdoin.edu/health/"}]
     },
     {
         title: "course evaluations",
         subtitle: "view or submit course evaluations",
-        links: [{label: "BCQs", link: "https://foxsylv1.github.io"}]
+        links: [{label: "BCQs", link: "https://blackboard.bowdoin.edu/webapps/portal/execute/tabs/tabAction?tab_tab_group_id=_218_1"}]
     },
     {
         title: "Bearable",
         subtitle: "help improve Bearable!",
-        links: [{label: "suggestions", link: "https://foxsylv1.github.io"},
-                {label: "pull request", link: "https://foxsylv1.github.io/letter-getter"}]
+        links: [{label: "suggestions", link: "https://github.com/FoxSylv1/bearable/issues"},
+                {label: "pull request", link: "https://github.com/FoxSylv1/bearable/pulls"}]
     },
 ];
