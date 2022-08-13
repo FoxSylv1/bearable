b62f1ada66b43d276229981917be3a51207dfca2
FoxSylv <atheriault@bowdoin.edu>
2022-08-12 23:17:35 -0400

    Add labels to all entries.
    NOTE! The links are not yet set up properly and
    currently lead to just my website.

--- src/data/entryData.js
+++ src/data/entryData.js
@@ -1,20 +1,79 @@
 export const entryData = [
     {
-        title: "title",
-        subtitle: "subtitle",
-        links: [{label: "link1", link: "https://foxsylv1.github.io"},
-                {label: "link2", link: "https://foxsylv1.github.io/letter-getter"}]
+        title: "classwork",
+        subtitle: "view and submit schoolwork",
+        links: [{label: "canvas", link: "https://foxsylv1.github.io"},
+                {label: "blackboard", link: "https://foxsylv1.github.io/letter-getter"}]
     },
     {
-        title: "title2",
-        subtitle: "subtitle2",
-        links: [{label: "link3", link: "https://foxsylv1.github.io"},
-                {label: "link4", link: "https://foxsylv1.github.io/letter-getter"}]
+        title: "outlook",
+        subtitle: "view and send email",
+        links: [{label: "email", link: "https://foxsylv1.github.io"},
+                {label: "calendar", link: "https://foxsylv1.github.io/letter-getter"}]
     },
     {
-        title: "title3",
-        subtitle: "subtitle3",
-        links: [{label: "link5", link: "https://foxsylv1.github.io"},
-                {label: "link6", link: "https://foxsylv1.github.io/letter-getter"}]
-    }
+        title: "onecard",
+        subtitle: "view your onecard data",
+        links: [{label: "meal swipes", link: "https://foxsylv1.github.io"},
+                {label: "money stored", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "housing & mail",
+        subtitle: "view your current residence/mailing address, or apply for housing",
+        links: [{label: "current", link: "https://foxsylv1.github.io"},
+                {label: "apply", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "dining services",
+        subtitle: "view dining hall menus and hours",
+        links: [{label: "menu", link: "https://foxsylv1.github.io"},
+                {label: "hours", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "community",
+        subtitle: "view what others are up to",
+        links: [{label: "campus groups", link: "https://foxsylv1.github.io"},
+                {label: "groupme", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "past courses",
+        subtitle: "view your transcript or request an audit",
+        links: [{label: "transcript", link: "https://foxsylv1.github.io"},
+                {label: "audit", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "registration",
+        subtitle: "view and register for courses",
+        links: [{label: "register", link: "https://foxsylv1.github.io"},
+                {label: "class listing", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "financial services",
+        subtitle: "view payment and financial aid information",
+        links: [{label: "bill", link: "https://foxsylv1.github.io"},
+                {label: "financial aid", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "labor services",
+        subtitle: "submit time or view labor opportunities",
+        links: [{label: "submit time", link: "https://foxsylv1.github.io"},
+                {label: "apply", link: "https://foxsylv1.github.io/letter-getter"}]
+    }, 
+    {
+        title: "health",
+        subtitle: "view supports for your well-being",
+        links: [{label: "mental", link: "https://foxsylv1.github.io"},
+                {label: "physical", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
+    {
+        title: "course evaluations",
+        subtitle: "view or submit course evaluations",
+        links: [{label: "BCQs", link: "https://foxsylv1.github.io"}]
+    },
+    {
+        title: "Bearable",
+        subtitle: "help improve Bearable!",
+        links: [{label: "suggestions", link: "https://foxsylv1.github.io"},
+                {label: "pull request", link: "https://foxsylv1.github.io/letter-getter"}]
+    },
 ];
